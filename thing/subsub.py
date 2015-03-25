8ee454ee08f9673a1841a672e76168bf1a0bc76c
meejah <meejah@meejah.ca>
2015-03-25 13:29:53 -0600

    fix subsub process coverage with dummy, do-nothing signal handler

--- thing/subsub.py
+++ thing/subsub.py
@@ -1,4 +1,11 @@
 import time
+import signal
+
+# do-nothing signal handler ensures that this process has "a" TERM
+# handler in Python, which allows coverage's atexit() function to
+# run...
+signal.signal(signal.SIGTERM, lambda a, b: None)
+
 
 print "subsub"
 time.sleep(5)
